e5580a82e5b91ca30ed782e2a6ca16ae073fe553
Skywt2003 <Skywt2003@users.noreply.github.com>
2024-08-18 08:30:05 +0000

    deploy: 235f029b25f81a04394939f60f2eee2492bad9d0

--- _astro/hoisted.B7CfB7Zh.js
+++
@@ -1,1 +0,0 @@
-import{O as a}from"./index.esm.CohKk5fn.js";import"./hoisted.BMqMAa2A.js";document.addEventListener("astro:page-load",()=>{a.bind("[data-fancybox]",{})},{once:!1});
---
+++ _astro/hoisted.Dqwyr6z_.js
@@ -0,0 +1,1 @@
+import{O as a}from"./index.esm.CohKk5fn.js";import"./hoisted.Bnwyq7gc.js";document.addEventListener("astro:page-load",()=>{a.bind("[data-fancybox]",{})},{once:!1});
